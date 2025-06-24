b39001da5c89c6ee954eedcf05fee00385762c48
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2025-06-24 17:29:49 +0300

    Update terminal_window.h

--- helwan-terminal/src/terminal_window.h
+++ helwan-terminal/src/terminal_window.h
@@ -2,16 +2,17 @@
 #define HELWAN_TERMINAL_WINDOW_H
 
 #include <gtk/gtk.h>
-#include <gobject/gobject.h> // Explicitly include gobject for G_DECLARE_FINAL_TYPE
+#include <gobject/gobject.h> // Include gobject for G_DECLARE_FINAL_TYPE
 #include <vte/vte.h>         // Include VTE header for VteTerminal type
 
 // G_DECLARE_FINAL_TYPE generates the necessary type registration functions
-// and casting macros (like HELWAN_TERMINAL_WINDOW) automatically.
-// The 5th argument should be the C struct name of the parent type, which is 'GtkWindow'.
+// and casting macros (like HELWAN_TERMINAL_WINDOW).
+// IMPORTANT: The 5th argument MUST be the C struct name of the parent type ('GtkWindow').
 G_DECLARE_FINAL_TYPE(HelwanTerminalWindow, helwan_terminal_window, HELWAN, TERMINAL_WINDOW, GtkWindow)
 
 // Define the structure for your custom window explicitly
-// This completes the 'typedef' definition for the compiler
+// This completes the 'typedef' definition for the compiler,
+// allowing access to members like 'notebook'.
 struct _HelwanTerminalWindow {
     GtkWindow parent_instance; // GtkWindow is the direct parent instance
     GtkWidget *notebook;       // Added for managing tabs
